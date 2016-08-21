3a11eb5f6f36d87354f4b72d67583baa0687b01c
EliteFrax <javier1@live.cl>
2016-08-21 12:23:43 -0300

    top_ten finalizado y termina rapido

--- funciones.py
+++ funciones.py
@@ -7,9 +7,9 @@ def Creacion_Diccionarios(ratings, movies):
 	for line in ratings_file:
 		usuario,movie,rating,time = line.strip().split('::')
 		if usuario in diccionario_usuarios_info_peliculas:
-			diccionario_usuarios_info_peliculas[usuario].append((movie,rating,time))
+			diccionario_usuarios_info_peliculas[usuario].append([movie,rating])
 		else:
-			diccionario_usuarios_info_peliculas[usuario] = [[movie,rating,time]]
+			diccionario_usuarios_info_peliculas[usuario] = [[movie,rating]]
 	ratings_file.close()
 	diccionario_datos_peliculas = dict()
 	movies_file = open(movies)
@@ -59,14 +59,16 @@ def Rating_Pelicula(usuario, pelicula, diccionario_usuarios_info_peliculas, dicc
 	suma_rat = 0
 	suma_rat_div = 0
 	for Espectador in diccionario_usuarios_info_peliculas:
+		if Espectador == usuario:
+			continue
 		for datos_ratings_usuario in diccionario_usuarios_info_peliculas[Espectador]:
 			if datos_ratings_usuario[0] == pelicula:
 				sim = Similitud_Entre_Usuarios(usuario, Espectador, diccionario_usuarios_info_peliculas, diccionario_datos_peliculas)
-				for mov,rat,time in diccionario_usuarios_info_peliculas[Espectador]:
-					if mov  ==  pelicula:
-						rat_spec = float(rat)
-						suma_rat += sim * rat_spec
-						suma_rat_div += float(sim)
+				for mov,rat in diccionario_usuarios_info_peliculas[Espectador]:
+					suma_rat += sim * float(rat)
+					suma_rat_div += sim
+	if suma_rat_div == 0:
+		return None
 	return suma_rat / suma_rat_div
 	
 def Peliculas_Sin_Rating(usuario, diccionario_usuarios_info_peliculas, diccionario_datos_peliculas):
@@ -80,14 +82,28 @@ def Peliculas_Sin_Rating(usuario, diccionario_usuarios_info_peliculas, diccionar
 def Top_Ten(usuario, diccionario_usuarios_info_peliculas, diccionario_datos_peliculas):
 	#la funcion que debe retornar las 10 mejores peliculas
 	candidatos = Peliculas_Sin_Rating(usuario, diccionario_usuarios_info_peliculas, diccionario_datos_peliculas)
+	usuarios_similares = Usuarios_Similares(usuario, diccionario_usuarios_info_peliculas, diccionario_datos_peliculas)
 	top_ten_previo = []
 	top_ten = []
 	for pelicula in candidatos:
-		print pelicula
-		rating = Rating_Pelicula(usuario, pelicula, diccionario_usuarios_info_peliculas, diccionario_datos_peliculas)
-		top_ten_previo.append((rating, pelicula))
-	top_ten.sort()
+		rating = Rating_Pelicula(usuario, pelicula, usuarios_similares, diccionario_datos_peliculas)
+		if rating != None:
+			top_ten_previo.append((rating, pelicula))
+	top_ten_previo.sort()
+	top_ten_previo.reverse()
 	for x in range(10):
 		top_ten.append(top_ten_previo[x])
-	return top_ten	
-		
+	return top_ten
+	
+def Usuarios_Similares(usuario, diccionario_usuarios_info_peliculas, diccionario_datos_peliculas):
+	similares = dict(diccionario_usuarios_info_peliculas)
+	a_eliminar = set()
+	for usuario_comparado in similares:
+		simil = Similitud_Entre_Usuarios(usuario, usuario_comparado, diccionario_usuarios_info_peliculas, diccionario_datos_peliculas)
+		if simil >= float(0.5):
+			continue
+		else:
+			a_eliminar.add(usuario_comparado)
+	for usuario_ido in a_eliminar:
+		del similares[usuario_ido]
+	return similares
--- main.py
+++ main.py
@@ -5,7 +5,12 @@ archmovies = 'movies.dat'
 
 #definicion de variables estaticas
 diccionario_usuarios_info_peliculas , diccionario_datos_peliculas = funciones.Creacion_Diccionarios(archratings, archmovies)
+usuario = '1'
+usuario1 = '1'
+usuario2 = '71568'
 
-
-print funciones.Rating_Pelicula('123','12', diccionario_usuarios_info_peliculas, diccionario_datos_peliculas)
-print funciones.Peliculas_Sin_Rating('123', diccionario_usuarios_info_peliculas, diccionario_datos_peliculas)
+#print funciones.Rating_Pelicula('123','12', diccionario_usuarios_info_peliculas, diccionario_datos_peliculas)
+#print funciones.Peliculas_Sin_Rating('123', diccionario_usuarios_info_peliculas, diccionario_datos_peliculas)
+print funciones.Top_Ten(usuario, diccionario_usuarios_info_peliculas, diccionario_datos_peliculas)
+#print funciones.Similitud_Entre_Usuarios(usuario1, usuario2, diccionario_usuarios_info_peliculas, diccionario_datos_peliculas)
+#print funciones.Usuarios_Similares(usuario, diccionario_usuarios_info_peliculas, diccionario_datos_peliculas)
